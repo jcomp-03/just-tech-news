7364889aabba2254aaa6eb7aa3905fb4e267dbed
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-03-22 16:34:35 -0400

    Created Post model

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Post model
+class Post extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      post_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // using Sequelize's built-in data validation
+        validate: {
+          isURL: true
+        }
+      },
+      // this field holds the foreign key reference in user model
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
+    }
+  );
+
+  module.exports = Post;
